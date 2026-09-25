25e6f6c5c99c027fb333952f9ae3ba30edf67d7e
watashi-00 <94692745+watashi-00@users.noreply.github.com>
2026-09-25 19:07:13 -0300

    feat(native_functions): add sys_munmap/sys_mprotect prototypes

--- include/native_functions.h
+++ include/native_functions.h
@@ -12,17 +12,21 @@ long sys_read(int fd, void *buf, long count);
 long sys_mmap(void *addr, long length, long prot,
               long flags, long fd, long offset);
 
+long sys_munmap(void *addr, long length);
+
+long sys_mprotect(void *addr, long length, long prot);
+
 long println_n(const char *str, long len);
 
-#define println(str) println_n(str, sizeof(str))
 
 long scan_n(void *buf, long count);
 
-#define scan(buf) scan_n(buf, sizeof(buf))
 
 #define bool _Bool
 #define true 1
 #define false 0
+#define scan(buf) scan_n(buf, sizeof(buf))
+#define println(str) println_n(str, sizeof(str))
 
 bool string_equals(const char *a, const char *b);
 
